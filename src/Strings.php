b5f253fd7aadae105cd7e7ed6d9c36bb8023d180
Awilum <awilum@msn.com>
2020-08-29 18:47:01 +0300

    fix coding standards

--- src/Strings.php
+++ src/Strings.php
@@ -4,6 +4,23 @@ declare(strict_types=1);
 
 namespace Flextype\Component\Strings;
 
+use function ctype_lower;
+use function implode;
+use function lcfirst;
+use function mb_strimwidth;
+use function mb_strlen;
+use function mb_strpos;
+use function mb_strtolower;
+use function mb_strtoupper;
+use function mb_strwidth;
+use function preg_match;
+use function preg_replace;
+use function random_int;
+use function rtrim;
+use function str_replace;
+use function trim;
+use function ucwords;
+
 class Strings
 {
     /**
@@ -17,9 +34,8 @@ class Strings
      * Removes any leading and traling slashes from a string.
      *
      * @param  string $string String with slashes
-     * @return string
      */
-    public static function trimSlashes(string $string) : string
+    public static function trimSlashes(string $string): string
     {
         return trim($string, '/');
     }
@@ -28,9 +44,8 @@ class Strings
      * Reduces multiple slashes in a string to single slashes.
      *
      * @param  string $string String or array of strings with slashes
-     * @return string
      */
-    public static function reduceSlashes(string $string) : string
+    public static function reduceSlashes(string $string): string
     {
         return preg_replace('#(?<!:)//+#', '/', $string);
     }
@@ -39,22 +54,20 @@ class Strings
      * Removes single and double quotes from a string.
      *
      * @param  string $str String with single and double quotes
-     * @return string
      */
-    public static function stripQuotes(string $string) : string
+    public static function stripQuotes(string $string): string
     {
-        return str_replace(array('"', "'"), '', $string);
+        return str_replace(['"', "'"], '', $string);
     }
 
     /**
      * Convert single and double quotes to entities.
      *
      * @param  string $string String with single and double quotes
-     * @return string
      */
-    public static function quotesToEntities(string $string) : string
+    public static function quotesToEntities(string $string): string
     {
-        return str_replace(array("\'", "\"", "'", '"'), array("&#39;", "&quot;", "&#39;", "&quot;"), $str);
+        return str_replace(["\'", '"', "'", '"'], ['&#39;', '&quot;', '&#39;', '&quot;'], $str);
     }
 
     /**
@@ -62,19 +75,18 @@ class Strings
      *
      * @param  int    $length   The number of characters. Default is 16
      * @param  string $keyspace The keyspace
-     * @return string
      */
-    public static function random(int $length = 64, string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') : string
+    public static function random(int $length = 64, string $keyspace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'): string
     {
         if ($length <= 0) {
             $length = 1;
         }
 
         $pieces = [];
-        $max = mb_strlen($keyspace, '8bit') - 1;
+        $max    = mb_strlen($keyspace, '8bit') - 1;
 
         for ($i = 0; $i < $length; ++$i) {
-            $pieces []= $keyspace[random_int(0, $max)];
+            $pieces[] = $keyspace[random_int(0, $max)];
         }
 
         return implode('', $pieces);
@@ -83,26 +95,24 @@ class Strings
     /**
      * Add's _1 to a string or increment the ending number to allow _2, _3, etc.
      *
-     * @param  string  $string    String to increment
-     * @param  int     $first     Start with
-     * @param  string  $separator Separator
-     * @return string
+     * @param  string $string    String to increment
+     * @param  int    $first     Start with
+     * @param  string $separator Separator
      */
-    public static function increment(string $string, int $first = 1, string $separator = '_') : string
+    public static function increment(string $string, int $first = 1, string $separator = '_'): string
     {
-        preg_match('/(.+)'. $separator . '([0-9]+)$/', $string, $match);
+        preg_match('/(.+)' . $separator . '([0-9]+)$/', $string, $match);
 
-        return isset($match[2]) ? $match[1] . $separator.($match[2] + 1) : $string . $separator . $first;
+        return isset($match[2]) ? $match[1] . $separator . ($match[2] + 1) : $string . $separator . $first;
     }
 
     /**
      * Return the length of the given string.
      *
-     * @param  string       $string   String to check
-     * @param  string|null  $encoding String encoding
-     * @return int
+     * @param  string      $string   String to check
+     * @param  string|null $encoding String encoding
      */
-    public static function length(string $string, $encoding = null) : int
+    public static function length(string $string, ?string $encoding = null): int
     {
         if ($encoding) {
             return mb_strlen($string, $encoding);
@@ -112,14 +122,13 @@ class Strings
     }
 
     /**
-      * Limit the number of characters in a string.
-      *
-      * @param  string  $string String
-      * @param  int     $limit  Limit of characters
-      * @param  string  $append Text to append to the string IF it gets truncated
-      * @return string
-      */
-    public static function limit(string $string, int $limit = 100, string $append = '...') : string
+     * Limit the number of characters in a string.
+     *
+     * @param  string $string String
+     * @param  int    $limit  Limit of characters
+     * @param  string $append Text to append to the string IF it gets truncated
+     */
+    public static function limit(string $string, int $limit = 100, string $append = '...'): string
     {
         if (mb_strwidth($string, 'UTF-8') <= $limit) {
             return $string;
@@ -132,9 +141,8 @@ class Strings
      * Convert the given string to lower-case.
      *
      * @param  string $string String
-     * @return string
      */
-    public static function lower(string $string) : string
+    public static function lower(string $string): string
     {
         return mb_strtolower($string, 'UTF-8');
     }
@@ -142,21 +150,19 @@ class Strings
     /**
      * Convert the given string to upper-case.
      *
-     * @param  string  $value
-     * @return string
+     * @param  string $value
      */
-    public static function upper(string $string) : string
+    public static function upper(string $string): string
     {
         return mb_strtoupper($string, 'UTF-8');
     }
 
-    /**
-     * Convert a string to studly caps case.
-     *
-     * @param  string  $string String
-     * @return string
-     */
-    public static function studly(string $string) : string
+        /**
+         * Convert a string to studly caps case.
+         *
+         * @param  string $string String
+         */
+    public static function studly(string $string): string
     {
         $key = $string;
 
@@ -169,14 +175,13 @@ class Strings
         return static::$cache['studly'][$key] = str_replace(' ', '', $string);
     }
 
-    /**
-     * Convert a string to snake case.
-     *
-     * @param  string  $string    String
-     * @param  string  $delimiter Delimeter
-     * @return string
-     */
-    public static function snake(string $string, string $delimiter = '_') : string
+        /**
+         * Convert a string to snake case.
+         *
+         * @param  string $string    String
+         * @param  string $delimiter Delimeter
+         */
+    public static function snake(string $string, string $delimiter = '_'): string
     {
         $key = $string;
 
@@ -187,7 +192,7 @@ class Strings
         if (! ctype_lower($string)) {
             $value = preg_replace('/\s+/u', '', ucwords($string));
 
-            $string = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1'.$delimiter, $string));
+            $string = static::lower(preg_replace('/(.)(?=[A-Z])/u', '$1' . $delimiter, $string));
         }
 
         return static::$cache['snake'][$key][$delimiter] = $string;
@@ -196,10 +201,9 @@ class Strings
     /**
      * Convert a string to camel case.
      *
-     * @param  string  $string String
-     * @return string
+     * @param  string $string String
      */
-    public static function camel($string)
+    public static function camel(string $string): string
     {
         if (isset(static::$cache['camel'][$string])) {
             return static::$cache['camel'][$string];
@@ -211,10 +215,9 @@ class Strings
     /**
      * Convert a string to kebab case.
      *
-     * @param  string  $string String
-     * @return string
+     * @param  string $string String
      */
-    public static function kebab($string)
+    public static function kebab(string $string): string
     {
         return static::snake($string, '-');
     }
@@ -222,30 +225,28 @@ class Strings
     /**
      * Limit the number of words in a string.
      *
-     * @param  string  $string String
-     * @param  int     $words  Words limit
-     * @param  string  $append Text to append to the string IF it gets truncated
-     * @return string
+     * @param  string $string String
+     * @param  int    $words  Words limit
+     * @param  string $append Text to append to the string IF it gets truncated
      */
-    public static function words(string $string, int $words = 100, string $append = '...') : string
+    public static function words(string $string, int $words = 100, string $append = '...'): string
     {
-        preg_match('/^\s*+(?:\S++\s*+){1,'.$words.'}/u', $string, $matches);
+        preg_match('/^\s*+(?:\S++\s*+){1,' . $words . '}/u', $string, $matches);
 
         if (! isset($matches[0]) || static::length($string) === static::length($matches[0])) {
             return $string;
         }
 
-        return rtrim($matches[0]).$append;
+        return rtrim($matches[0]) . $append;
     }
 
     /**
      * Determine if a given string contains a given substring.
      *
-     * @param  string           $haystack The string being checked.
-     * @param  string|string[]  $needles  The string to find in haystack.
-     * @return bool
+     * @param  string          $haystack The string being checked.
+     * @param  string|string[] $needles  The string to find in haystack.
      */
-    public static function contains(string $haystack, $needles) : bool
+    public static function contains(string $haystack, $needles): bool
     {
         foreach ((array) $needles as $needle) {
             if ($needle !== '' && mb_strpos($haystack, $needle) !== false) {
